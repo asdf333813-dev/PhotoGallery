26626cc331cc892c629f043d47986230abd5ce6a
Андрей <asdf333813@gmail.com>
2026-06-14 14:42:33 +0700

    Add PhotoGallery base screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,15 +6,13 @@ plugins {
 android {
     namespace = "com.example.photogallery"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.example.photogallery"
         minSdk = 27
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
